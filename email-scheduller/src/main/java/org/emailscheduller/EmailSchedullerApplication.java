ac979dba98403f21071e9b042b73fea50c689242
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2021-11-01 17:55:09 +0300

    Initial commit

---
+++ email-scheduller/src/main/java/org/emailscheduller/EmailSchedullerApplication.java
@@ -0,0 +1,13 @@
+package org.emailscheduller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSchedullerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSchedullerApplication.class, args);
+    }
+
+}
---
+++ email-scheduller/src/test/java/org/emailscheduller/EmailSchedullerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emailscheduller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSchedullerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
